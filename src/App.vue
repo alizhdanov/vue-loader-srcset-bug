66861363727c56b57bb1acc97180c551dd9151ba
Ali Zhdanov <ali.zhdanov@bistudio.com>
2017-11-10 15:02:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h3>NOT WORKING</h3>
+
+    <picture>
+      <source srcset="http://www.gstatic.com/webp/gallery/1.webp">
+      <source srcset="http://www.gstatic.com/webp/gallery/1.jpg">
+      <img src="http://www.gstatic.com/webp/gallery/1.jpg" alt="test image">
+    </picture>
+
+    <h3>WORKING</h3>
+
+    <picture>
+      <source srcset="http://www.gstatic.com/webp/gallery/2.webp 1x">
+      <source srcset="http://www.gstatic.com/webp/gallery/2.jpg 1x">
+      <img src="http://www.gstatic.com/webp/gallery/2.jpg" alt="test image">
+    </picture>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
